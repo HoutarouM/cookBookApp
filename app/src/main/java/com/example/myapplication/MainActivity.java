e00bdaba261e4b62114283b90e4fd4a0a3c015bc
HoutarouM <marian.horoshko@gmail.com>
2022-11-17 08:07:54 +0100

    add layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    // jezeli w ukladzie z ogranieczeniami nie zdefiniowane ograniczenia (constrainty)
+    // pojawia sie w lewych gornym rogu
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
